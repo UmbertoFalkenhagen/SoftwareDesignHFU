06e5ff1bc49e5856619625e76e201b08e65adbb2
Umberto <umberto.falkenhagen@hs-furtwangen.de>
2020-05-19 15:47:33 +0200

    Tasks completed

---
+++ Debugging Übung/Übung_Debugging.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Die ist eine Ausgabe!");
+console.warn("Dies ist ein Warnung!");
+console.info("Dies ist eine Info");
+console.error("Dies ist ein Fehler!");
+//# sourceMappingURL=Übung_Debugging.js.map
---
+++ Debugging Übung/Übung_Debugging.ts
@@ -0,0 +1,4 @@
+console.log("Die ist eine Ausgabe!");
+console.warn("Dies ist ein Warnung!");
+console.info("Dies ist eine Info");
+console.error("Dies ist ein Fehler!"):
